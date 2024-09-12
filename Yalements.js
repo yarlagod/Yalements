3a256c9437834bbae71e8eabe6c0f4d29b1281bb
yarlagod <122803207+yarlagod@users.noreply.github.com>
2024-09-12 15:54:09 +0300

    Add files via upload

---
+++ Yalements.js
@@ -0,0 +1,63 @@
+class btn extends HTMLElement {
+  static observedAttributes = ["width","height","color","hover-color","text-color","font","radius"];
+
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    if (this.hasAttribute("height")){
+      var height = this.getAttribute("height");
+    }
+    else{
+      var height = "fit-content";
+    }
+    if (this.hasAttribute("width")){
+      var width = this.getAttribute("width");
+    }
+    else{
+      var width = "fit-content";
+    }
+    if (this.hasAttribute("color")){
+      var color = this.getAttribute("color");
+    }
+    else{
+      var color = "#eee";
+    }
+    if (this.hasAttribute("hover-color")){
+      var hcolor = this.getAttribute("hover-color");
+    }
+    else{
+      var hcolor = "#ccc";
+    }
+    if (this.hasAttribute("click-color")){
+      var ccolor = this.getAttribute("click-color");
+    }
+    else{
+      var ccolor = "#999";
+    }
+    if (this.hasAttribute("text-color")){
+      var tcolor = this.getAttribute("text-color");
+    }
+    else{
+      var tcolor = "#000";
+    }
+    if (this.hasAttribute("font")){
+      var font = this.getAttribute("font");
+    }
+    else{
+      var font = "Arial";
+    }
+    if (this.hasAttribute("radius")){
+      var rad = this.getAttribute("radius");
+    }
+    else{
+      var rad = "100px";
+    }
+    this.setAttribute("style","display: block;user-select: none;cursor: pointer;padding: 10px;text-align: center;border-radius: "+rad+";width: "+width+";height: "+height+";line-height:"+height+";background-color: "+color+";color: "+tcolor+";font-family: "+font+";")
+    this.setAttribute("onmouseover","this.style.backgroundColor = '"+hcolor+"';")
+    this.setAttribute("onmouseout","this.style.backgroundColor = '"+color+"';")
+    this.setAttribute("onmousedown","this.style.backgroundColor = '"+ccolor+"';")
+    this.setAttribute("onmouseup","this.style.backgroundColor = '"+hcolor+"';")
+  }
+}
+customElements.define("styled-button", btn);
